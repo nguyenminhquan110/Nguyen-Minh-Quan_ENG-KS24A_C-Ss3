34d254f7764384e6a77a9a2ed998ee672f15bb91
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-21 20:53:20 +0700

    Add files via upload

---
+++ Ss3_3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float r, chuVi, dienTich;
+    printf("Nhap ban kinh hinh tron (r): ");
+    scanf("%f", &r);
+    chuVi = 2 * M_PI * r;
+    dienTich = M_PI * r * r;
+    printf("Chu vi hinh tron là: %.2f\n", chuVi);
+    printf("Dien tich hinh tron là: %.2f\n", dienTich);
+    return 0;
+}
